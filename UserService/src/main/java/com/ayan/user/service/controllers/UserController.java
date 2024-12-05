7afa54d46f4cbde44319d0eeee8da461b2fff670
Ayan Ghorai <ayan9ghorai@gmail.com>
2024-12-05 11:34:46 +0530

    spring security modification

--- UserService/src/main/java/com/ayan/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/ayan/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.ayan.user.service.controllers;
 
 import com.ayan.user.service.entities.User;
 import com.ayan.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,12 +27,31 @@ public class UserController
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
 
+    int retryCount = 1;
+
     // single user get
     @GetMapping("/{userId}")
+    // @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    // @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId)
     {
+        System.out.println("Retry count : " + retryCount);
+        retryCount++;
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    // creating fallback method for circuit breaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
+        System.out.println("Fallback executed because service is down : " + ex.getMessage());
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("Dummy user created because of some service is down")
+                .userId("123456")
+                .build();
+        return new ResponseEntity<>(user , HttpStatus.OK);
     }
 
     // all user get
